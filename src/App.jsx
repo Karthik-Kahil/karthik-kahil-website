54533918d3ba1ca51b535b0b0a4ab89aecb9f091
Karthik Kahil <karthikkahil@Karthiks-MacBook-Air.local>
2023-10-08 16:09:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+function App() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a href="">Work</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Play</a>
+          </li>
+          <li>
+            <a href="">Notes</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
